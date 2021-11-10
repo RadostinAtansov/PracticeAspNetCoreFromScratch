22369e055ccf2a35f8b47b93ceea583410dca083
Radostin Atanasov <hapa_21@abv.bg>
2021-11-10 20:05:51 +0200

    global Exceptionhandling

---
+++ PracticeAspNetCoreWithKunvenkat/Controllers/ErrorController.cs
@@ -0,0 +1,38 @@
+﻿
+namespace PracticeAspNetCoreWithKunvenkat.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Components.Routing;
+    using Microsoft.AspNetCore.Diagnostics;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ErrorController : Controller
+    {
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            switch(statusCode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "Sory, the page could not be found";
+                    break;
+            }
+            return View("NotFound");
+        }
+
+
+        [AllowAnonymous]
+        [Route("Error")]
+        public IActionResult Error()
+        {
+            var exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            ViewBag.ExceptionPath = exceptionHandlerPathFeature.Path;
+            ViewBag.ExceptionMessage = exceptionHandlerPathFeature.Error.Message;
+            ViewBag.StackTrace = exceptionHandlerPathFeature.Error.StackTrace;
+
+            return View("Error");
+
+        }
+    }
+}
