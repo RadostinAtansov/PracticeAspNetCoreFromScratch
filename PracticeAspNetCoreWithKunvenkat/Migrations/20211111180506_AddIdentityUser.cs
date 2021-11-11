51a04a365f616e66b705ad40e16548862730e7c2
Radostin Atanasov <hapa_21@abv.bg>
2021-11-11 20:09:49 +0200

    AddIdentity User

---
+++ PracticeAspNetCoreWithKunvenkat/Migrations/20211111180506_AddIdentityUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PracticeAspNetCoreWithKunvenkat.Migrations
+{
+    public partial class AddIdentityUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
